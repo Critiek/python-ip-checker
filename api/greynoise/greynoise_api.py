9f8800b6338683a67b49ce766590786cf7f988dc
Critiek <jumbra99@gmail.com>
2023-09-18 21:49:40 +0930

    made dedicated keys folder

--- api/greynoise/greynoise_api.py
+++ api/greynoise/greynoise_api.py
@@ -1,6 +1,6 @@
 import requests
 
-with open('api/greynoise/greynoise_key.txt', 'r') as file:
+with open('api/keys/greynoise_key.txt', 'r') as file:
     key = file.read()
 
 headers = {"key": key, "accept": "application/json"}
